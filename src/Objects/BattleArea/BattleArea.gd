4003520e274760528036d0615c8a40ab4b917dce
Caresle <carabesle@gmail.com>
2022-08-14 13:51:42 -0600

    Fix typo in BattleArea

--- src/Objects/BattleArea/BattleArea.gd
+++ src/Objects/BattleArea/BattleArea.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 
 func _on_BattleArea_body_entered(_body: Node) -> void:
-	get_tree().change_scene("res://src/Test/BattleZone.tscn".tscn")
+	get_tree().change_scene("res://src/Test/BattleZone.tscn")
